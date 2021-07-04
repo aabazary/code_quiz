e84f56dd6568ba5a52ed326031ec630c4c6cea2a
aabazary <aabazary@gmail.com>
2021-07-04 01:59:26 -0700

    revereted to old logic, quiz is running, needs css/penalty and HS

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,70 +1,104 @@
-// //questions from the gif image
-// var questions = [{
-//         title: "Commonly used data types DO NOT include:",
-//         choices: ["strings", "booleans", "alerts", "numbers"],
-//         answer: "alerts"
-//     },
-//     {
-//         title: "The condition in an if / else statement is enclosed within ____.",
-//         choices: ["quotes", "curly brackets", "parentheses", "square brackets"],
-//         answer: "parentheses"
-//     },
-//     {
-//         title: "Arrays in Javascript can be used to store ____.",
-//         choices: ["numbers and strings", "other arrays", "booleans", "all of the above"],
-//         answer: "all of the above"
-//     },
-//     {
-//         title: "String values must be enclosed within ____ when being assigned to variables.",
-//         choices: ["commas", "curly brackets", "quotes", "parenthesis"],
-//         answer: "quotes"
-//     },
-//     {
-//         title: "A very useful tool used during development and debugging for printing content to the debugger is:",
-//         choices: ["Javascript", "terminal / bash", "for loops", "console log"],
-//         answer: "console log"
-//     },
+//questions from the gif image
+var questions = [{
+        title: "Commonly used data types DO NOT include:",
+        choices: ["strings", "booleans", "alerts", "numbers"],
+        answer: "alerts"
+    },
+    {
+        title: "The condition in an if / else statement is enclosed within ____.",
+        choices: ["quotes", "curly brackets", "parentheses", "square brackets"],
+        answer: "parentheses"
+    },
+    {
+        title: "Arrays in Javascript can be used to store ____.",
+        choices: ["numbers and strings", "other arrays", "booleans", "all of the above"],
+        answer: "all of the above"
+    },
+    {
+        title: "String values must be enclosed within ____ when being assigned to variables.",
+        choices: ["commas", "curly brackets", "quotes", "parenthesis"],
+        answer: "quotes"
+    },
+    {
+        title: "A very useful tool used during development and debugging for printing content to the debugger is:",
+        choices: ["Javascript", "terminal / bash", "for loops", "console log"],
+        answer: "console log"
+    },
 
-// ];
-// var questionIndex = 0;
-// var questionDiv = document.querySelector('#question');
-// var startButton = document.querySelector('#start-button')
-// var remainingTime = document.querySelector('#timer')
+];
+var questionIndex = 0;
+var startButton = document.querySelector('#start-button')
+var remainingTime = document.querySelector('#timer')
 
-// var secondsLeft = 1000;
-// var holdInterval = 0;
-// var penalty = 10;
+var secondsLeft = 1000;
+var holdInterval = 0;
+var penalty = 10;
 
-// var questionIndex = 0;
+var questionIndex = 0;
+var ulCreate = document.createElement("ul");
 
-// startButton.addEventListener("click", function () {
-//     if (holdInterval === 0) {
-//         holdInterval = setInterval(function () {
-//             secondsLeft--;
-//             remainingTime.textContent = "Time: " + secondsLeft;
+startButton.addEventListener("click", function () {
+    if (holdInterval === 0) {
+        holdInterval = setInterval(function () {
+            secondsLeft--;
+            remainingTime.textContent = "Time: " + secondsLeft;
 
-//             if (secondsLeft <= 0) {
-//                 clearInterval(holdInterval);
-//                 remainingTime.textContent = "Time's up!";
-//             }
-//         }, 1000);
-//     }
+            if (secondsLeft <= 0) {
+                clearInterval(holdInterval);
+                remainingTime.textContent = "Time's up!";
+            }
+        }, 1000);
+    }
+    startQuiz(questionIndex); 
  
-// });
-var startButton = document.getElementById('start-button')
+});
 
-startButton.addEventListener('click', startGame)
-setQuestion()
-setAnswer()
-function startGame(){
-    startButton.classList.add('hide')
-    console.log(startButton)
+function startQuiz(questionIndex) {
+    
+    questionsDiv.innerHTML = "";
+    ulCreate.innerHTML = "";
+    
+    for (var i = 0; i < questions.length; i++) {
+       
+        var userQuestion = questions[questionIndex].title;
+        var userChoices = questions[questionIndex].choices;
+        questionsDiv.textContent = userQuestion;
+    }
+  
+    userChoices.forEach(function (newItem) {
+        var listItem = document.createElement("li");
+        listItem.textContent = newItem;
+        questionsDiv.appendChild(ulCreate);
+        ulCreate.appendChild(listItem);
+        listItem.addEventListener("click", (compare));
+    })
 }
 
-function setQuestion(){
+function compare(event) {
+    var element = event.target;
 
-}
+    if (element.matches("li")) {
 
-function setAnswer(){
+        var createDiv = document.createElement("div");
+        createDiv.setAttribute("id", "createDiv");
+
+        if (element.textContent == questions[questionIndex].answer) {
+            createDiv.textContent = "Correct! The answer is:  " + questions[questionIndex].answer;
+
+        } else {
+          
+            createDiv.textContent = "Incorrect! The correct answer is:  " + questions[questionIndex].answer;
+        }
+
+    }
+
+    questionIndex++;
+
+    if (questionIndex == questions.length) {
+        createDiv.textContent = "End of quiz!";
+    } else {
+        startQuiz(questionIndex);
+    }
+    questionsDiv.appendChild(createDiv);
 
 }
